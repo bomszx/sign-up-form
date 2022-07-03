9af17b76e757608fcaed714fd53592e1a3b50748
bon nocos <bon.nocos@gmail.com>
2022-07-03 20:29:34 +0800

    finalized styles

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-// alert('hello world')
+// DOM Elements
+
+let pw = document.querySelector('#passWord').innerText;
+let confirmPw = document.querySelector('#confirmPw');
+
+// Password Variables
+let initialPw;
+let checkPw;
+
+// Event Listeners
+
+// pw.addEventListener('keydown', function(e) {
+//     console.log(e)
+// })
+
+confirmPw.addEventListener('keydown', function(e) {
+    console.log(e)
+})
+
+console.log(pw)
